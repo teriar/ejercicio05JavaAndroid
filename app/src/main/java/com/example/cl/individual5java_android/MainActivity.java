5c975320491b59c00302e892a050f11377b47f0a
Felipe Gonzalez <felipegonzalezurr@gmail.com>
2023-06-19 21:01:28 -0400

    funcional botton toast

--- app/src/main/java/com/example/cl/individual5java_android/MainActivity.java
+++ app/src/main/java/com/example/cl/individual5java_android/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.cl.individual5java_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initListeners();
+    }
+
+    private void initListeners() {
+
+        Button showToast = findViewById(R.id.buttonShowToast);
+        Button counter = findViewById(R.id.buttonCounter);
+        TextView textView = findViewById(R.id.textView);
+        showToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "hello toast", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
