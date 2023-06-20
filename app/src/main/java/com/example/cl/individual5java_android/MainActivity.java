84f152b1761a902c409fcfec4f75d14a8350529e
Felipe Gonzalez <felipegonzalezurr@gmail.com>
2023-06-19 20:32:45 -0400

    commit inicial, ya establecido el diseño (layout)

---
+++ app/src/main/java/com/example/cl/individual5java_android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.cl.individual5java_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
